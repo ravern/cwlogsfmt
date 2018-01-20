6882498b4632af9ec23b45830ca71d05215f5444
Ravern Koh <ravern.koh.dev@gmail.com>
2018-01-21 01:29:06 +0800

    It's WARNING not WARN

--- cwlogfmt.go
+++ cwlogfmt.go
@@ -18,7 +18,7 @@ import (
 //     PANIC RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: A serious crash
 //     FATAL RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: A crash
 //     ERROR RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: An error
-//     WARN RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: A warning
+//     WARNING RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: A warning
 //     INFO RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: Some information
 //     DEBUG RequestId: 66389135-fd00-11e7-a1f9-8945479469b0 Message: Some more information
 //     END RequestId: 66389135-fd00-11e7-a1f9-8945479469b0
